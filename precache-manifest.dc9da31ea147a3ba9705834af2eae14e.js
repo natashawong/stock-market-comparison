4d28c9aa1f1a551fbc77e2254c825937f8660a3d
natashawong <natasha.wong10@gmail.com>
2020-08-14 16:00:51 +0800

    Updates

---
+++ precache-manifest.dc9da31ea147a3ba9705834af2eae14e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc298a35491b89204cef970ff3890ff5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0de044d2556c26bb10f2",
+    "url": "/static/css/main.d77950cd.chunk.css"
+  },
+  {
+    "revision": "0dcca441ca6cdbeddd45",
+    "url": "/static/js/2.3e53f72a.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/static/js/2.3e53f72a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0de044d2556c26bb10f2",
+    "url": "/static/js/main.ade65183.chunk.js"
+  },
+  {
+    "revision": "cfa5cba5009259b1675c",
+    "url": "/static/js/runtime-main.e8716490.js"
+  }
+]);
