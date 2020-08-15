b2d069cb926df1b481f1a3c3086f82c7cecc3792
natashawong <natasha.wong10@gmail.com>
2020-08-15 23:02:06 +0800

    change homepage from "link" to "." because no routing, maybe this will fix "Failed to load source...404" error

---
+++ build/precache-manifest.1d130305ab99b04de8a643943f047c2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f76f6805b0fdea8446b4ae720fdaf21c",
+    "url": "/stock-market-comparison/index.html"
+  },
+  {
+    "revision": "c193fe3434c77f9f97ec",
+    "url": "/stock-market-comparison/static/css/main.d77950cd.chunk.css"
+  },
+  {
+    "revision": "0dcca441ca6cdbeddd45",
+    "url": "/stock-market-comparison/static/js/2.3e53f72a.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/stock-market-comparison/static/js/2.3e53f72a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c193fe3434c77f9f97ec",
+    "url": "/stock-market-comparison/static/js/main.9688243c.chunk.js"
+  },
+  {
+    "revision": "6ba36b3f2c3693e05b2c",
+    "url": "/stock-market-comparison/static/js/runtime-main.46c7154c.js"
+  }
+]);
